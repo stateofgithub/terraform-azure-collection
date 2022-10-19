e5e316bea7b65ee539e9403f2062333d2edf59a2
Jiayuan Chen <jyc@observeinc.com>
2022-10-19 01:29:11 +0000

    implement most utils

--- AzureFunctionAppDev/EventHubTriggerPythonDev/__init__.py
+++ AzureFunctionAppDev/EventHubTriggerPythonDev/__init__.py
@@ -1,64 +1,28 @@
 #! /usr/local/bin/python3
 
-import asyncio
 import azure.functions as func
-import functools
-import logging
-import requests
 import json
-import os
-import io
+import logging
 
 from datetime import datetime
-from requests.adapters import HTTPAdapter
-from azure.identity import DefaultAzureCredential
+from observe.utils import BaseHandler
 
-OBSERVE_CLIENT = None
+EVENTHUB_HANDLER = None
 
 
-class ObserveClient:
+class EventHubHandler(BaseHandler):
     def __init__(self):
-        # Required environment variables.
-        try:
-            self.observe_customer = os.environ["OBSERVE_CUSTOMER"]
-            self.observe_token = os.environ["OBSERVE_TOKEN"]
-            self.observe_domain = os.environ["OBSERVE_DOMAIN"]
-        except:
-            logging.critical(
-                "[ObserveClient] Required ENV_VARS are not set properly")
-            exit(-1)
+        super().__init__()
+        self.source = "eventhub_triggered_func"
+        self._reset_state()
 
-        # Optional environment variables with default value.
-        self.max_req_size_byte = int(
-            os.getenv("OBSERVE_CLIENT_MAX_REQ_SIZE_BYTE") or 512*1024)
-        # Each request to Observe can batch multiple events from Event Hub,
-        # and each event can contains multiple observation records.
-        self.max_events_per_req = int(
-            os.getenv("OBSERVE_CLIENT_MAX_EVENTS_PER_REQ") or 256)
-        self.max_retries = int(os.getenv("OBSERVE_CLIENT_MAX_RETRIES") or 5)
-        self.max_timeout_sec = int(
-            os.getenv("OBSERVE_CLIENT_MAX_TIMEOUT_SEC") or 10)
-
-        logging.info(
-            f"[ObserveClient] Initialized a new client: "
-            f"observe_customer = {self.observe_customer}, "
-            f"domain = {self.observe_domain}, "
-            f"max_req_size_byte = {self.max_req_size_byte}, "
-            f"max_events_per_req = {self.max_events_per_req}")
-
-    async def reset_state(self):
-        self.num_obs_in_cur_req = 0
-        self.num_events_in_cur_req = 0
-        self.buf = io.StringIO()
-        self.buf.write("[")
-
-    async def process_events(self, event_arr: func.EventHubEvent):
+    async def process_events(self, event_arr) -> None:
         """
         Parse and send all incoming events to Observe, in multiple requests
         if needed. Each HTTP request contains an array of JSON observations.
         """
         if len(event_arr) == 0:
-            logging.error("[ObserveClient] 0 event to process, skip")
+            logging.error("[EventHubHandler] 0 event to process, skip")
             return
 
         # For cardinality=many scenarios, each event points to the common
@@ -67,17 +31,13 @@ class ObserveClient:
             raise Exception(
                 "Event metadata is missing from function invocation")
 
-        # Index of the starting event in the current Observe request.
-        event_index = 0
-        await self.reset_state()
-
+        self._reset_state()
+        self.buf.write("[")
         for e in event_arr:
             try:
                 raw_data = json.loads(e.get_body().decode())
             except:
                 raise Exception("Event data is not a valid JSON")
-
-            self.num_events_in_cur_req += 1
             # Data could be a list of records in the format of {"records": [...]}
             # In this case, we break it down into multiple observations.
             is_json_list = ("records" in raw_data and type(
@@ -85,106 +45,34 @@ class ObserveClient:
             for r in raw_data["records"] if is_json_list else [raw_data]:
                 self.buf.write(json.dumps(r, separators=(',', ':')))
                 self.buf.write(",")
-                self.num_obs_in_cur_req += 1
+                self.num_obs += 1
+                # Buffer size is above threshold.
+                if self.buf.tell() >= self.max_req_size_byte:
+                    self.event_metadata = event_arr[0].metadata
+                    await self._wrap_buffer_and_send_request()
+                    self._reset_state()
+                    self.buf.write("[")
 
-            if self.num_events_in_cur_req >= self.max_events_per_req or \
-                    self.buf.tell() >= self.max_req_size_byte:
-                self.buf.write(
-                    await self._build_req_metadata_json(
-                        event_arr[0].metadata, event_index))
-                # Update the start index for the next request.
-                event_index += self.num_events_in_cur_req
-                self.buf.write("]")
-                await self._send_request()
-                await self.reset_state()
+        if self.num_obs > 0:
+            self.event_metadata = event_arr[0].metadata
+            await self._wrap_buffer_and_send_request()
+            self._reset_state()
 
-        if self.num_obs_in_cur_req > 0:
-            self.buf.write(await self._build_req_metadata_json(
-                event_arr[0].metadata, event_index))
-            self.buf.write("]")
-            await self._send_request()
-            await self.reset_state()
-
-        logging.info(f"[ObserveClient] {len(event_arr)} events processed.")
-
-    async def _build_req_metadata_json(self, event_metadata, event_index) -> str:
-        """
-        Construct a metadata observation for the current Observe request,
-        as the last record in the JSON array.
-        """
-        if self.buf.tell() <= 0:
-            raise Exception("Buffer should contain data but is empty")
-        elif self.num_obs_in_cur_req <= 0:
-            raise Exception(
-                "There should be at least 1 observation in current request")
-
-        req_meta = {
-            "ObserveNumEvents": self.num_events_in_cur_req,
-            "ObserveNumObservations": self.num_obs_in_cur_req,
-            "ObserveTotalSizeByte": self.buf.tell(),
-            "ObserveSubmitTimeUtc": str(datetime.utcnow()).replace(' ', 'T'),
-            "AzureEventHubPartitionContext": event_metadata.get("PartitionContext", {}),
-        }
-
-        if "SystemPropertiesArray" not in event_metadata:
-            req_meta["AzureEventHubSystemPropertiesArray"] = {}
-        else:
-            req_meta["AzureEventHubSystemPropertiesArray"] = event_metadata[
-                "SystemPropertiesArray"][event_index: event_index+self.num_obs_in_cur_req]
-
-        return json.dumps(req_meta, separators=(',', ':'))
-
-    async def _send_request(self):
-        """
-        Wrap up the request, and send the observations in the current buffer to
-        Observe's collector endpoint.
-        """
-        if self.buf.tell() <= 0:
-            raise Exception("Buffer should contain data but is empty")
-
-        if os.getenv("DEBUG_OUTPUT") == 'true':
-            # Instead of sending data to Observe endpoint, print it out for debugging.
-            logging.critical(f"[ObserveClient] {self.buf.getvalue()}")
-            return
-
-        # Send the request.
-        req_url = f"https://{self.observe_customer}.collect.{self.observe_domain}/v1/http/azure?source=EventHubTriggeredFunction"
-        s = requests.Session()
-        s.mount(req_url, HTTPAdapter(max_retries=self.max_retries))
-
-        loop = asyncio.get_event_loop()
-        future = loop.run_in_executor(
-            None,
-            functools.partial(
-                s.post,
-                req_url,
-                headers={
-                    'Authorization': 'Bearer ' + self.observe_token,
-                    'Content-type': 'application/json'},
-                data=bytes(self.buf.getvalue().encode('utf-8')),
-                timeout=self.max_timeout_sec)
-        )
-        response = await future
-
-        # Error handling.
-        response.raise_for_status()
-
-        logging.info(
-            f"[ObserveClient] {self.num_obs_in_cur_req} observations sent, "
-            f"response: {response.json()}")
+        logging.info(f"[EventHubHandler] {len(event_arr)} events processed.")
 
 
 async def main(event: func.EventHubEvent):
-    # Create an HTTP client, or load it from the cache.
-    global OBSERVE_CLIENT
-    if OBSERVE_CLIENT is None:
-        OBSERVE_CLIENT = ObserveClient()
+    # Create a new eventhub handler, or load it from the cache.
+    global EVENTHUB_HANDLER
+    if EVENTHUB_HANDLER is None:
+        EVENTHUB_HANDLER = EventHubHandler()
 
     try:
-        await OBSERVE_CLIENT.process_events(event)
+        await EVENTHUB_HANDLER.process_events(event)
     except Exception as e:
-        logging.critical(f"[ObserveClient] {str(e)}")
+        logging.critical(f"[EventHubHandler] {str(e)}")
         exit(-1)
     except:
-        logging.critical("[ObserveClient] Unknown error processing the events")
+        logging.critical(
+            "[EventHubHandler] Unknown error processing the events")
         exit(-1)
--- AzureFunctionAppDev/TimerTriggerPythonDev/__init__.py
+++ AzureFunctionAppDev/TimerTriggerPythonDev/__init__.py
@@ -2,129 +2,91 @@
 
 import logging
 import azure.functions as func
-import logging
-import requests
-import logging
 import json
-import io
 import os
 
+from datetime import datetime
 from azure.mgmt.resource import ResourceManagementClient
-from requests.adapters import HTTPAdapter
-from azure.identity import DefaultAzureCredential, ClientSecretCredential\
+from azure.identity import ClientSecretCredential
 
-import azure.functions as func
+from observe.utils import BaseHandler
+
+
+RESOURCES_HANDLER = None
 
 SUBSCRIPTION_ID = "2B514E36-3A9B-4C89-AD51-4BDCAB22E84F"
 
+# TODO:
+# make the function async io
+# refactor the code to use handler - buffer - observe client
+# get list of subscriptions, and list the resources in all of them.
+# permissions: how to work around the AZURE_TENANT_ID, AZURE_CLIENT_ID, and APP stuff?
+# "only collected resources that we are collecting data for": how do we apply filter, and keep the filter up to date?
+# request id
+# rename "EventHubTriggerPythonDev" and "TimerTriggerPythonDev"
+# make sure my changes are compatible with the rest of the
+# add comment to all function and classes.
 
-def list_all_resources() -> io.StringIO:
-    # Required environment variables.
-    try:
-        azure_tenant_id = os.environ["AZURE_TENANT_ID"]
-        azure_client_id = os.environ["AZURE_CLIENT_ID"]
-        azure_client_secret = os.environ["AZURE_CLIENT_SECRET"]
-    except:
-        logging.critical(
-            "[ObserveClient] Required ENV_VARS are not set properly")
-        exit(-1)
 
-    credentials = ClientSecretCredential(
-        tenant_id=azure_tenant_id, client_id=azure_client_id,
-        client_secret=azure_client_secret)
-    client = ResourceManagementClient(credentials, SUBSCRIPTION_ID)
+class ResourcesHandler(BaseHandler):
+    def __init__(self):
+        super().__init__()
+        self.source = "timer_triggered_func"
+        self._reset_state()
 
-    buf = io.StringIO()
-    buf.write("[")
-    for idx, item in enumerate(client.resources.list(expand='createdTime,changedTime,provisioningState')):
-        if idx != 0:
-            buf.write(",")
-        buf.write(json.dumps(item.serialize(
-            keep_readonly=True), separators=(',', ':')))
-    buf.write("]")
-    return buf
+        # Required environment variables.
+        try:
+            self.azure_tenant_id = os.environ["AZURE_TENANT_ID"]
+            self.azure_client_id = os.environ["AZURE_CLIENT_ID"]
+            self.azure_client_secret = os.environ["AZURE_CLIENT_SECRET"]
+        except:
+            logging.critical(
+                "[ResourcesHandler] Required ENV_VARS are not set properly")
+            exit(-1)
+
+    async def list_resources(self) -> None:
+        # Construct Azure credentials.
+        credentials = ClientSecretCredential(
+            tenant_id=self.azure_tenant_id,
+            client_id=self.azure_client_id,
+            client_secret=self.azure_client_secret)
+        client = ResourceManagementClient(credentials, SUBSCRIPTION_ID)
+
+        self._reset_state()
+        self.buf.write("[")
+        for resource in client.resources.list(expand='createdTime,changedTime,provisioningState'):
+            self.buf.write(json.dumps(resource.serialize(
+                keep_readonly=True), separators=(',', ':')))
+            self.buf.write(",")
+            self.num_obs += 1
+            # Buffer size is above threshold.
+            if self.buf.tell() >= self.max_req_size_byte:
+                await self._wrap_buffer_and_send_request()
+                self._reset_state()
+                self.buf.write("[")
+
+        if self.num_obs > 0:
+            await self._wrap_buffer_and_send_request()
+            self._reset_state()
+
+        logging.info(f"[ResourcesHandler] Resources processed.")
 
 
 async def main(mytimer: func.TimerRequest) -> None:
-    # Create an HTTP client, or load it from the cache.
-    global OBSERVE_CLIENT
-    if OBSERVE_CLIENT is None:
-        OBSERVE_CLIENT = ObserveClient()
+    # Create a new resources handler, or load it from the cache.
+    global RESOURCES_HANDLER
+    if RESOURCES_HANDLER is None:
+        RESOURCES_HANDLER = ResourcesHandler()
 
-    # if mytimer.past_due:
-        # logging.info('The timer is past due!')
+    if mytimer.past_due:
+        logging.info('The timer is past due!')
 
     try:
-        OBSERVE_CLIENT._send_request(list_all_resources())
-
+        await RESOURCES_HANDLER.list_resources()
     except Exception as e:
-        logging.critical(f"[ObserveClient] {str(e)}")
+        logging.critical(f"[ResourcesHandler] {str(e)}")
         exit(-1)
     except:
-        logging.critical("[ObserveClient] Unknown error processing the events")
+        logging.critical(
+            "[ResourcesHandler] Unknown error processing the resources")
         exit(-1)
-
-
-# remove duplicated code
-
-OBSERVE_CLIENT = None
-
-
-class ObserveClient:
-    def __init__(self):
-        # Required environment variables.
-        try:
-            self.observe_customer = os.environ["OBSERVE_CUSTOMER"]
-            self.observe_token = os.environ["OBSERVE_TOKEN"]
-            self.observe_domain = os.environ["OBSERVE_DOMAIN"]
-        except:
-            logging.critical(
-                "[ObserveClient] Required ENV_VARS are not set properly")
-            exit(-1)
-
-        # Optional environment variables with default value.
-        self.max_req_size_byte = int(
-            os.getenv("OBSERVE_CLIENT_MAX_REQ_SIZE_BYTE") or 512*1024)
-        # Each request to Observe can batch multiple events from Event Hub,
-        # and each event can contains multiple observation records.
-        self.max_events_per_req = int(
-            os.getenv("OBSERVE_CLIENT_MAX_EVENTS_PER_REQ") or 256)
-        self.max_retries = int(os.getenv("OBSERVE_CLIENT_MAX_RETRIES") or 5)
-        self.max_timeout_sec = int(
-            os.getenv("OBSERVE_CLIENT_MAX_TIMEOUT_SEC") or 10)
-
-        logging.info(
-            f"[ObserveClient] Initialized a new client: "
-            f"observe_customer = {self.observe_customer}, "
-            f"domain = {self.observe_domain}, "
-            f"max_req_size_byte = {self.max_req_size_byte}, "
-            f"max_events_per_req = {self.max_events_per_req}")
-
-    def _send_request(self, input_buffer) -> None:
-        """
-        Wrap up the request, and send the observations in the current buffer to
-        Observe's collector endpoint.
-        """
-        if input_buffer.tell() <= 0:
-            raise Exception("Buffer should contain data but is empty")
-
-        if os.getenv("DEBUG_OUTPUT") == 'true':
-            # Instead of sending data to Observe endpoint, print it out for debugging.
-            logging.critical(f"[ObserveClient] {input_buffer.getvalue()}")
-            return
-
-        # Send the request.
-        req_url = f"https://{self.observe_customer}.collect.{self.observe_domain}/v1/http/azure?source=EventHubTriggeredFunction"
-        s = requests.Session()
-        s.mount(req_url, HTTPAdapter(max_retries=self.max_retries))
-
-        response = s.post(
-            req_url,
-            headers={
-                'Authorization': 'Bearer ' + self.observe_token,
-                'Content-type': 'application/json'},
-            data=bytes(input_buffer.getvalue().encode('utf-8')),
-            timeout=self.max_timeout_sec)
-
-        # Error handling.
-        response.raise_for_status()
