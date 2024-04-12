aff41f1866ba008ee93e6880af418c1c3d38c0cf
Nikhil Dua <nikhil.dua@observeinc.com>
2024-04-12 15:24:49 -0700

    try outouts

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,11 @@ output "eventhub_namespace_id" {
   description = "Resource ID of the eventhub namespace used for Observe collection."
   value       = azurerm_eventhub_namespace.observe_eventhub_namespace.id
 }
+
+output "observe_customer" {
+  value = var.observe_customer
+}
+
+output "observe_location" {
+  value = var.location 
+}
