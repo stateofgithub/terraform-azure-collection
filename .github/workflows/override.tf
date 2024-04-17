27fb84e5d4c9f7939b8eff473d824491d774aed6
Nikhil Dua <nikhil.dua@observeinc.com>
2024-04-17 12:08:54 -0700

    update or

--- .github/workflows/override.tf
+++ .github/workflows/override.tf
@@ -1,13 +1,8 @@
-# variable "branch_name" {
-#   type = string
-# }
-
-
 terraform {
   backend "azurerm" {
     resource_group_name = "rg-terraform-github-actions-state"
     storage_account_name = "citeststfazurecollection"
     container_name       = "tfstate"
-    key                  = "${var.branch_name}/.tfstate"
+    key                  = "${{ steps.extract_branch.outputs.branch }}/.tfstate"
   }
 }
