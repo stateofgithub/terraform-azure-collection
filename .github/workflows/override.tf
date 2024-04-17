1d3bc1dea8dd440bc9fc21ba7a0cabc190c80f21
Nikhil Dua <nikhil.dua@observeinc.com>
2024-04-17 12:10:02 -0700

    rmv override

--- .github/workflows/override.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name = "rg-terraform-github-actions-state"
-    storage_account_name = "citeststfazurecollection"
-    container_name       = "tfstate"
-    key                  = "${{ steps.extract_branch.outputs.branch }}/.tfstate"
-  }
-}
