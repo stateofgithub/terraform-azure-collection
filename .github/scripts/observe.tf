e986cf714d5ac211d3f1c816af08c02c13f13c30
Nikhil Dua <nikhil.dua@observeinc.com>
2024-04-18 14:11:23 -0700

    try observe

---
+++ .github/scripts/observe.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    observe = {
+      source  = "terraform.observeinc.com/observeinc/observe"
+     version = ">= 0.13.3"
+    }
+  }
+}
+
+# Configure the observe provider
+provider "observe" {}
+
+# Look up existing workspace 
+data "observe_workspace" "default" {
+  name = "Default"
+}
