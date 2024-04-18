65aef7fa8e2cc5ca9f9405a14a4a2b7274a4a36e
Nikhil Dua <nikhil.dua@observeinc.com>
2024-04-18 14:49:00 -0700

    try observe

--- .github/scripts/observe.tf
+++ .github/scripts/observe.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_providers {
+    observe = {
+      source  = "terraform.observeinc.com/observeinc/observe"
+      version = ">= 0.13.3"
+    }
+  }
+  required_version = ">= 1.0"
+}
+
 # Configure the observe provider
 provider "observe" {}
 
