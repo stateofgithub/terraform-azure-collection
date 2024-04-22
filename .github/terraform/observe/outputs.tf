f0483bb234dd85a5e2465c750878945c2b811cb3
Nikhil Dua <nikhil.dua@observeinc.com>
2024-04-22 15:30:30 -0700

    add addnl outputs

--- .github/terraform/observe/outputs.tf
+++ .github/terraform/observe/outputs.tf
@@ -3,3 +3,13 @@ output "observe_token" {
   value       = observe_datastream_token.github_actions_branch_token.secret
   sensitive   = true
 }
+
+output "observe_token_id" {
+  description = "Observe Token ID for Ephemeral Branch"
+  value       = observe_datastream_token.github_actions_branch_token.id 
+}
+
+output "azure_datastream_token" {
+  description = "Azure Datastream"
+  value       = data.observe_datastream.azure.id 
+} 
