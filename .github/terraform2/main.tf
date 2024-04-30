10d52f9d13875808271e8b4906ae7f4bf5cd116a
Nikhil Dua <nikhil.dua@observeinc.com>
2024-04-30 14:34:27 -0700

    Update comments and notes

--- .github/terraform2/main.tf
+++ .github/terraform2/main.tf
@@ -11,7 +11,8 @@ terraform {
 }
 
 
-###  OBSERVE TOKEN SETUP ####
+########  OBSERVE TOKEN SETUP ########
+############################################
 
 # Uses Github Actions Environment Variables for below 
 # OBSERVE_CUSTOMER
@@ -39,8 +40,11 @@ resource "observe_datastream_token" "github_actions_branch_token" {
   datastream = data.observe_datastream.azure.oid
   name       = var.branch
 }
+#############################################
+
 
 ##  AZURE TF COLLECTION ## 
+#############################################
 module "terraform-azure-collection" {
   source                  = "../../"
   observe_customer        = var.observe_customer
@@ -49,3 +53,5 @@ module "terraform-azure-collection" {
   observe_token           = observe_datastream_token.github_actions_branch_token.secret
   function_app_debug_logs = true
 }
+
+##############################################
